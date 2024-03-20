bbef462ca12c7243ecb9dc7a156ebbf6b90e80ac
Ashbin-M-Abraham <133525700+Ashbin-M-Abraham@users.noreply.github.com>
2024-03-20 10:37:46 -0400

    dom

--- lesson-10/js/json.js
+++ lesson-10/js/json.js
@@ -17,9 +17,9 @@ const requestURl = "https://smccrindle.github.io/comp1073module10/js/i-scream.js
     const iScream = await response.json();
     
     // STEP 8: Output the iScream JSON object to the console 
-    console.log
+    console.log(iScream);
     // STEP 9a: Invoke the populateHeader function here, then build it below
-    
+    populateHeader(iScream);
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
     
 }
@@ -28,13 +28,13 @@ const requestURl = "https://smccrindle.github.io/comp1073module10/js/i-scream.js
 populate();
 
 /* STEP 9b: Build out the populateHeader() function */
-function populateHeader() {
+function populateHeader(jsonObj) {
     // Create the H1 element
-    
+    let headerH1 = document.createElement( 'h1' );
     // Grab the company name from the JSON object and use it for the text node
-    
+    headerH1.textContent = jsonObj.CompanyName;
     // Inject the complete H1 element into the DOM, inside the HEADER
-    
+    header.appendChild(headerH1);
 };
 /* STEP 10b: Assemble the showTopFlavors() function */
 function showTopFlavors() {
